f515f8fe6920c889fd2ee0910d398357399a5b72
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-08-06 05:59:50 +0200

    v2.0
    - Code rewriting and improvement.
    - Now client/server compatible.
    - New informations added to the HUD about models caching with a progress bar and percent.

---
+++ improved_precache_system/lua/autorun/ipr_cache_system_sh.lua
@@ -0,0 +1,148 @@
+--- Script By Inj3
+--- Script By Inj3
+--- Script By Inj3
+--- https://steamcommunity.com/id/Inj3/
+local ipr_cache = {} --- Do not touch !
+--- Version 2.0
+
+----- // Configuration 
+ipr_cache.delay = 0.3 --- Délai entre chaque mise en cache. / Delay between caching.
+ipr_cache.enable_clientside = true --- Activer la mise en cache côté client / Enable client-side caching.
+ipr_cache.enable_serverside = true --- Activer la mise en cache côté server / Enable server-side caching.
+
+ipr_cache.blacklist = { --- Indiquer ici les véhicules à ne pas inclure en cache. / Include here the vehicles not to be included in the cache.
+    ["Yacht_2"] = true, --- Exemple / Example - Ne pas inclure dans le cache si celui-ci n'apparaît jamais sur le serveur. / Do not include in the cache if it never appears on the server.
+    ["airtugtdm"] = true,
+}
+ipr_cache.modelsys = { --- Indiquer ici vos models customs à ajouter dans le cache. / Enter your custom models to be added to the cache here.
+    --"models/bkeypads/keypad_3.0.0.mdl", --- Exemple / Example
+    --"models/bkeypads/keycard.mdl",
+}
+ipr_cache.customsound = { --- Indiquer ici vos sons customs à ajouter dans le cache. / Enter your custom sounds to be added to the cache here.
+    --"bKeypads.ScanningPing", --- Exemple / Example
+}
+----- //
+
+--- Do not touch anything below.
+---
+local ipr_count, ipr_delay, ipr_modelmax, ipr_modelprogress, ipr_load_caching = 0, 0, 0, ""
+
+local function Ipr_CacheModel()
+     local ipr_caching = {}
+     ipr_load_caching = true
+ 
+     ipr_caching.vehicle = {}
+     for c, d in pairs(list.Get("Vehicles")) do
+         if ipr_cache.blacklist[c] then
+             continue
+         end
+         ipr_caching.vehicle[#ipr_caching.vehicle + 1] = d.Model
+     end
+ 
+     ipr_caching.custom_model = ipr_cache.modelsys
+     ipr_caching.sound = ipr_cache.customsound
+
+     local ipr_c_custom_model, ipr_c_sound, ipr_c_vehicle = #ipr_caching.custom_model, #ipr_caching.sound, #ipr_caching.vehicle
+     ipr_modelmax = ipr_c_custom_model + ipr_c_sound + ipr_c_vehicle
+
+     if (ipr_modelmax == 0) then
+         return
+     end
+     local ipr_valid = (ipr_c_custom_model > 0) and (ipr_c_sound > 0) and 3 or ((ipr_c_custom_model > 0) or (ipr_c_sound > 0)) and 2 or 1
+ 
+     local ipr_cp = 0
+     for t, m in pairs(ipr_caching) do
+         if not m then
+             continue
+         end
+ 
+         for n, v in ipairs(m) do
+             ipr_delay = ipr_delay + ipr_cache.delay
+ 
+             timer.Simple(ipr_delay, function()
+                 ipr_modelprogress = tostring(v)
+                 if (t == "sound") then util.PrecacheSound(v) else util.PrecacheModel(v) end
+                 print("Model caching : " ..v)
+ 
+                 if (n == #m) then
+                     ipr_cp = ipr_cp + 1
+                     print("Caching completed : " ..t.. "\n")
+ 
+                     if not CLIENT then
+                         return
+                     end
+                     if (ipr_valid == ipr_cp) then
+ 
+                         timer.Simple(0.5, function()
+                             ipr_load_caching = false
+                         end)
+ 
+                     end
+                 end
+                 ipr_count = ipr_count + 1
+             end)
+         end
+     end
+ 
+     print("Number of models to cache : " ..ipr_modelmax.. "\nEstimated time : " ..math.Round(ipr_modelmax * ipr_cache.delay, 1).. " secs\n----")
+end 
+
+if (SERVER) then
+     util.AddNetworkString("ipr_net_cachesys")
+     local ipr_cacheprevent = false
+ 
+     if (ipr_cache.enable_serverside) then
+         hook.Add("InitPostEntity", "Ipr_CachingInit", function()
+             if not ipr_cache.enable_serverside then
+                 return
+             end
+ 
+             if not ipr_cacheprevent then
+                 Ipr_CacheModel()
+ 
+                 ipr_cacheprevent = true
+             end
+         end)
+     end
+
+     hook.Add("PlayerInitialSpawn", "Ipr_CachingSpawn", function(ply)
+         if not ipr_cache.enable_clientside then
+             return
+         end
+ 
+         timer.Simple(5, function()
+             if not IsValid(ply) then
+                 return
+             end
+ 
+             net.Start("ipr_net_cachesys")
+             net.Send(ply)
+         end)
+     end)
+
+     print("Improved Caching System by Inj3")
+else
+     if not ipr_cache.enable_clientside then
+         return
+     end
+ 
+     net.Receive("ipr_net_cachesys", function()
+         Ipr_CacheModel()
+     end)
+ 
+     local ipr_blue_box = Color(0,69,175)
+     hook.Add("HUDPaint", "Ipr_CachingHud", function()
+         if not ipr_load_caching then
+             return
+         end
+         local ipr_loading_box = ipr_count / ipr_modelmax
+         local ipr_percent = math.Round(ipr_loading_box * 100)
+         local ipr_loading_box_clamp = math.Clamp(ipr_loading_box * 100, 0, 100)
+ 
+         draw.RoundedBox(1, ScrW() / 2 - 50, ScrH() / 2 + 13, 100, 10, color_white)
+         draw.RoundedBox(1, ScrW() / 2 - 50, ScrH() / 2 + 13, ipr_loading_box_clamp, 10, ipr_blue_box)
+ 
+         draw.SimpleText("Caching in progress : " ..ipr_count.. "/" ..ipr_modelmax, "DermaDefault", ScrW() / 2, ScrH() / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+         draw.SimpleText(ipr_percent.. "% - "..ipr_modelprogress, "DermaDefault", ScrW() / 2, ScrH() / 2 + 30, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+     end)
+end  
